6a1d3db0bdd714e079c948d57838a3c1f4d9b671
Diego Pais <drborges.pais@gmail.com>
2022-06-07 10:49:27 -0300

    feat: header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyle from './styles/global';
+import Header from './components/Header'
 import RoutesComponent from './routes';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <RoutesComponent />
       <GlobalStyle />
     </BrowserRouter>

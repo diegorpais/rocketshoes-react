8c1c260dbde659ab0db39f2dad9c4b2f3c9f6c22
Diego Pais <drborges.pais@gmail.com>
2022-06-07 10:15:40 -0300

    feat: global styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { BrowserRouter } from 'react-router-dom';
+
+import GlobalStyle from './styles/global';
 import RoutesComponent from './routes';
 
 function App() {
   return (
     <BrowserRouter>
       <RoutesComponent />
+      <GlobalStyle />
     </BrowserRouter>
   );
 }

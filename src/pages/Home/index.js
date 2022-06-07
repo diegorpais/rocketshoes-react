8ae27dc190bc33181ebe287ace4d97b36ec16ab0
Diego Pais <drborges.pais@gmail.com>
2022-06-07 12:50:19 -0300

    feat: home page styling

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,66 @@
-import React from 'react'
+import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import { ProductList } from './styles';
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <ProductList>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-mizuno-cometa/26/2FU-5849-026/2FU-5849-026_zoom1.jpg" alt="Tenis" />
+        <strong>Nome do tenis</strong>
+        <span>R$ 129,90</span>
+
+        <button type='button'>
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-mizuno-cometa/26/2FU-5849-026/2FU-5849-026_zoom1.jpg" alt="Tenis" />
+        <strong>Nome do tenis</strong>
+        <span>R$ 129,90</span>
+
+        <button type='button'>
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-mizuno-cometa/26/2FU-5849-026/2FU-5849-026_zoom1.jpg" alt="Tenis" />
+        <strong>Nome do tenis</strong>
+        <span>R$ 129,90</span>
+
+        <button type='button'>
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-mizuno-cometa/26/2FU-5849-026/2FU-5849-026_zoom1.jpg" alt="Tenis" />
+        <strong>Nome do tenis</strong>
+        <span>R$ 129,90</span>
+
+        <button type='button'>
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+
+    
+  );
 }
